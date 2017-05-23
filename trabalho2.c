e99180e27badf6b1eb2ea97fe189d4b1c6d4ca4c
Leonardo <l-t-alves@github.com>
2017-05-23 17:07:06 -0300

    Adicionado o cabecalho

--- trabalho2.c
+++ trabalho2.c
@@ -1,10 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
-#include"printPersonagem.h"
-#include"arvore.h"
-#include"criador.h"
+/*		Alunos:	Bruno Murta Oliveira de Castro e Leonardo Teixeira Alves
+ *	Matrículas:	16/0069742 e 16/0012007
+ *	Disciplina:	Estruturas de Dados - Turma F
+ *	Professor:	Marcos Fagundes Caetano
+ *	Assunto:	Trabalho 2 - Árvores genéticas */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "printPersonagem.h"
+#include "arvore.h"
+#include "criador.h"
 
 int main(){
 
